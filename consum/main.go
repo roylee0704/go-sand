0a39a229aa2888398b70bc35d6e30e651f0cf469
roylee0704 <roylee0704@gmail.com>
2017-07-23 12:40:14 +0800

    most efficient solve

--- consum/main.go
+++ consum/main.go
@@ -8,7 +8,27 @@ func main() {
 	list := []int{-1, 2, 4, -3, 5, 2, -5, 2}
 	best := slowest(list)
 	best = medium(list)
+	best = efficient(list)
+
 	fmt.Println("best:", best)
+}
+
+// o(n). largest sum: sequential-sum + array[point] > array[point]
+func efficient(list []int) int {
+	n := len(list)
+	best := 0
+	sum := 0
+	var collection []int
+	for i := 0; i < n; i++ {
+		sum = max(list[i], sum+list[i])
+		if sum == list[i] {
+			collection = nil
+		}
+		collection = append(collection, list[i])
+		best = max(sum, best)
+	}
+	fmt.Println(collection)
+	return best
 }
 
 //  o(n * n)
